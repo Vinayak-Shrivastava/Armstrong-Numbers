a47980218b4da05821f5d6a76f873b043b2b9e40
Vinayak Shrivastava <85111501+Vinayak-Shrivastava@users.noreply.github.com>
2021-08-01 13:27:50 +0530

    Update armstrong.c

--- armstrong.c
+++ armstrong.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #include<math.h>
 
-int number_of_digits( int n)
+int number_of_digits( int n)   // to calculate number of digits
 {
     int c=0;
-    while(n>0)
+    while(n>0)              //eliminates rightmost digits until the number becomes zero. The number of times this eleimination happens is equal to the number of digits
     {
         n=n/10;
         c++;
     }
-    return c;
+    return c;           //return number of digits
 }
 
 int digit_sum( int n )
@@ -17,10 +17,10 @@ int digit_sum( int n )
     int a=0,c = number_of_digits(n);
     while(n>0)
     {
-        a += pow(n%10,c);
-        n=n/10;
+        a += pow(n%10,c);                       // calculate sum of the nth powers of the digits of the number
+        n=n/10;                                //eliminating rightmost digit so that the immidate left digit become rightmost for next turn and will be extracted
     }
-    return a;
+    return a;                                //return the calculated sum
 }
 
 int main()
@@ -31,9 +31,9 @@ int main()
     printf("Armstrong numbers in the range [1,%d] : ",n);
     for(i=1;i<=n;i++)
     {
-       if( digit_sum(i) == i ) printf("%d ",i);
+       if( digit_sum(i) == i ) printf("%d ",i);                      // if the calculated sum is the number itself then the number is a Armstrong number and is thus printed.
 
     }
 
-    return 0;
+    return 0;                                                          
 }
